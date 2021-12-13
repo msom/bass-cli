51b3b8683f197b51c245a35d935d41a5b5ac4f83
Marc Sommerhalder <msom@gmx.ch>
2021-12-13 08:39:46 +0100

    Add chord progressions

--- chords.py
+++ chords.py
@@ -1,8 +1,8 @@
 from blessings import Terminal
 from getkey import getkey, keys
 from mingus.containers import Note
-from mingus.core.chords import from_shorthand, determine
-from mingus.core.progressions import skip, to_chords
+from mingus.core.chords import determine, from_shorthand
+from mingus.core.progressions import numerals, to_chords
 from mingus.extra.tunings import get_tuning
 
 
@@ -46,7 +46,6 @@ def print_fretboard(terminal, fretboard, base):
         return result
 
     for string in reversed(fretboard):
-        # todo: use color
         line = [' |-'] + 24 * ['------']
         for note in string:
             if note.fret:
@@ -56,24 +55,41 @@ def print_fretboard(terminal, fretboard, base):
         print(''.join(line))
 
 
+def input_chord():
+    choice = input('Enter a chord ("Am7") or a base and optional a '
+                   'progression ("A", "A I VIIdim7", "A"): ').split(' ')
+    choice = [item.strip() for item in choice if item.strip()]
+    chord = choice[0] if choice else ''
+    notes = from_shorthand(chord)
+    base = notes[0]
+    progression = choice[1:]
+    if not progression and notes == from_shorthand(base):
+        progression = numerals
+    return chord, notes, base, progression
+
+
 tuning = get_tuning('bass', 'standard')
 terminal = Terminal()
-chord = input('Enter chord: ')
-base = chord
-progression = 'I'
 key = None
 while key != 'q':
+    print(terminal.clear())
+
+    if key in ('r', None):
+        chord, notes, base, progression = input_chord()
+        index = 0
+        print(terminal.clear())
+
+    if key in (keys.LEFT, keys.RIGHT):
+        index = (index + (1 if key == keys.RIGHT else -1)) % len(progression)
+        new = to_chords([progression[index]], base)[0]
+        chord = determine(new, base, True)[0]
+
     notes = from_shorthand(chord)
     fretboard = get_fretboard(notes, tuning)
 
-    print(terminal.clear())
-    print(f'{chord} {progression}\n')
+    print(f'{base} {" ".join(progression)}\n')
+    print(f'{terminal.bold}{chord} {progression[index]} {terminal.normal}\n')
     print_fretboard(terminal, fretboard, notes[0])
-    print('')
-    print('Press "q" to quit, arrow up/down for scale progression')
+    print('\nPress "q" to quit, arrow for scale progression, "r" to restart')
 
     key = getkey()
-    if key in (keys.LEFT, keys.RIGHT):
-        progression = skip(progression, 1 if key == keys.RIGHT else -1)
-        new = to_chords([progression], base)[0]
-        chord = determine(new, base, True)[0]
